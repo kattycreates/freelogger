1a830dcd360ec134a6c94bd90706c3c4b6568f3e
kattycreates <iamkarthika7@gmail.com>
2022-04-09 19:31:35 +0530

    modified styles

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -6,25 +6,33 @@ const Post = ({post}) => {
   //const imagePath='http://localhost:5000/images/';
   //const imagePath='/images/';
   return (
-    <div className='post'>{post.postImage&&<img className='postImg' src={post.postImage} alt="" />}
+    <div className='post'>
+        <div className='postDiv'>
+            {post.postImage&&<img className='postImg' src={post.postImage} alt="" />}
 
+            
+            <div className='postInfo'>
+                
+                <h3 className='postTitle'><Link to={`/post/${post._id}`}>{post.title}</Link></h3>
+                {post.categories[0]!==''&&<div className="postCategories">
+                    <span>Categories:{post.categories.map((cat,index)=>{
+                        return <Link to={`/?cat=${cat}`} key={index}  className="postCat" ><b> {cat} </b></Link>
+                    })}</span>
+                </div>}
+                <span> Author : <Link to={`/?user=${post.username}`}><b>{post.username}</b></Link></span>
+                <div className="postDate">
+                    {new Date(post.createdAt).toLocaleString()}
+                </div>
+                <p className='postDesc'>{post.desc}</p>
         
-        <div className='postInfo'>
-            <div className="postCategories">
-              {post.categories.map((cat,index)=>{
-                  return <span key={index}><Link to={`/?cat=${cat}`}  className="postCat" ><b>{cat}</b></Link></span>
-              })}
             </div>
-            <h3><Link to={`/post/${post._id}`} className='postTitle'>{post.title}</Link></h3>
-            <span> Author : <Link to={`/?user=${post.username}`}><b>{post.username}</b></Link></span>
-            <div className="postDate">
-                {new Date(post.createdAt).toLocaleString()}
-            </div>
-            <p className='postDesc'>{post.desc}</p>
+
         </div>
+       
         <div className='readDiv'>
         <Link to={`/post/${post._id}`} className='read'>Read</Link>
         </div>
+        
 
     </div>
   )
