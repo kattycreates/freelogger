89f6d57d2f38de5fa78dfdebdfc4c18d57c975e9
kattycreates <iamkarthika7@gmail.com>
2022-04-10 13:27:40 +0530

    minor changes

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -5,7 +5,8 @@ const Footer = () => {
   return (
     <div className='footer' id='footer'>
         <h3 className='contact'>Contact me on</h3>
-        <p>Website: <a className='website' href='https://karthika-selvam.netlify.app/'>https://karthika-selvam.netlify.app/</a></p>
+        <h4>Website</h4>
+        <a className='website' href='https://karthika-selvam.netlify.app/'>https://karthika-selvam.netlify.app/</a>
         <h4>Socials</h4>
         <div className="footerIcons">
     
