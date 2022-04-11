947dad11e272543dae9729e6326381bdbaf82afd
kattycreates <iamkarthika7@gmail.com>
2022-04-11 12:00:23 +0530

    minor changes

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const cors=require('cors');
 const path=require('path');
 dotenv.config();
 
-const PORT=process.env.PORT||5000;
+
 
 app.use(express.json());
 app.use(cors());
@@ -47,4 +47,4 @@ app.get("*",(req,res)=>{
     res.sendFile(path.join(__dirname,"client","build","index.html"));
 });
 
-app.listen(PORT,()=>console.log('Running'));
+app.listen(process.env.PORT||5000,()=>console.log('Running'));
